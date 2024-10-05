4a3eeb01a85d07ef9f84023e4a5a9f71a9f78558
Nika Tukhashvili <nika.tukhashvili22@gmail.com>
2024-10-05 12:16:52 +0400

    add JsonIngore attribute. test commit.

--- Models/User.cs
+++ Models/User.cs
@@ -1,11 +1,13 @@
-﻿namespace Reddit.Models
+﻿using System.Text.Json.Serialization;
+
+namespace Reddit.Models
 {
     public class User
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
-
+        [JsonIgnore]
         public virtual List<Post> Posts { get; set; } = new();
         public virtual List<Comment> Comments { get; set; } = new();
     }
