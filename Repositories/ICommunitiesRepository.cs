48fadf30fba4a498816f4ba609305f8678ffa81e
Nika Tukhashvili <nika.tukhashvili22@gmail.com>
2024-11-22 01:23:16 +0400

    add search, pagination and filtering functionality to communities for GetCommunity endpoint

---
+++ Repositories/ICommunitiesRepository.cs
@@ -0,0 +1,11 @@
+﻿
+
+using Reddit.Models;
+
+namespace Reddit.Repositories
+{
+    public interface ICommunitiesRepository
+    {
+        public Task<PagedList<Community>> GetCommunities(int pageNumber, int pageSize, string? searchKey, string? sortKey = null, bool isAscending = true);
+    }
+}
